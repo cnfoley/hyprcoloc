7b6e5d41ec29a5ee8732822f45b036a5516840d8
ChrisF <chrisf@BSUs-MacBook-Pro.local>
2020-12-15 19:16:43 +0000

    separate out functions inside original hyprcoloc.R

--- R/hyprcoloc.R
+++ R/hyprcoloc.R
@@ -531,3 +531,16 @@ hyprcoloc <- function(effect.est, effect.se, binary.outcomes = rep(0, dim(effect
   return(results)
 
 }
+
+#' Print HyPrColoc
+#'
+#' print method for class "hyprcoloc"
+#' @param x an object of class "hyprcoloc"
+#' @author Christopher N Foley <chris.neal.foley@gmail.com> and James R Staley <jrstaley95@gmail.com>
+#' @export
+print.hyprcoloc <- function(x, ...){
+  cat("\nCall: \nhyprcoloc")
+  cat("\n\nResults:\n")
+  print(x$results)
+  cat("\n")
+}
